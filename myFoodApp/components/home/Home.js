deee812c2f0ef8355a30317fc52325068f28ea5a
Kurt Cancemi <kurt@x64architecture.com>
2020-04-14 12:02:00 -0400

    Fix bad merge

--- myFoodApp/components/home/Home.js
+++ myFoodApp/components/home/Home.js
@@ -42,7 +42,7 @@ render() {
         </View>
 
         <View style= {styles.iconView}>
-        <TouchableOpacity onPress={() => {alert("This will open the List Scanner Page");}}>
+        <TouchableOpacity onPress={() => { navigate("Scanner") }}>
         <Image style={styles.icons}
         source={require ('../../assets/grocery_list.png')}>
         </Image>
